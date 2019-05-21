839bc311ee2836dc5f91b9c67e661bad7618d72e
Julia Signell <jsignell@gmail.com>
2019-05-21 13:48:24 -0400

    Adding anaconda-project file (#8)
    * Adding anaconda-project file
    
    * Shortening comment
    
    * Renaming app to glaciers

--- panelserverextension.py
+++ panelserverextension.py
@@ -1,5 +1,5 @@
 from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
-    """serve the app.ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "app.ipynb", "--allow-websocket-origin=*"])
+    """serve the glaciers.ipynb directory with bokeh server"""
+    Popen(["panel", "serve", "glaciers.ipynb", "--allow-websocket-origin=*"])
