59921c9adddd5be071e07592af99597097a69aa7
Julia Signell <jsignell@gmail.com>
2019-10-21 14:56:48 -0400

    Using jupyter-panel-proxy (#11)
    * Trying to use new jupyter-panel-server
    
    * Fixing env spec
    
    * Changing package name to jupyter-panel-proxy
    
    * Fixing up README with new links
    
    * Changing name back to glaciers
    
    * Now that I've released to pypi

--- panelserverextension.py
+++
@@ -1,5 +0,0 @@
-from subprocess import Popen
-
-def load_jupyter_server_extension(nbapp):
-    """serve the glaciers.ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "glaciers.ipynb", "--allow-websocket-origin=*"])
